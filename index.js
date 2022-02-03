cd8a90adb7085b293641351c66c84c93bc87911e
Bridget Akellian <akellianbv@gmail.com>
2022-02-03 00:12:28 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+
+function saturdayFun(text) {
+    if (text === undefined) { 
+      text = 'roller-skate';
+    }
+    return `This Saturday, I want to ${text}!`
+  }
+
+saturdayFun("bathe my dog");
+
+function mondayWork(text) {
+    if (text === undefined) {
+        text = "go to the office";
+    }
+    return `This Monday, I will ${text}.`
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(string) {
+    if (string === undefined) {
+        string = "*";
+    }
+    return function(text) {
+        if (text === undefined) {
+            text = "special"
+        }
+        return `You are ${string}${text}${string}!`
+    }
+}
